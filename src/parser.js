41fc806ad1267449164433fb6d560755639638a2
Moritz Vifian <moritz.vifian@gmx.ch>
2024-02-23 00:00:26 +0100

    Bumping protobuf version
    using const instead of var

--- src/parser.js
+++ src/parser.js
@@ -4,53 +4,53 @@
  * protobuf text format.
  */
 
-var Parsimmon = require('parsimmon');
+const Parsimmon = require('parsimmon');
 
-var regex = Parsimmon.regex
+const regex = Parsimmon.regex
   , string = Parsimmon.string
   , optWhitespace = Parsimmon.optWhitespace
   , lazy = Parsimmon.lazy
   , alt = Parsimmon.alt
   , seq = Parsimmon.seq;
 
-var comment = regex(/#.+/).then(optWhitespace.atMost(1));
-var whitespace = optWhitespace.then(comment.atMost(1));
+const comment = regex(/#.+/).then(optWhitespace.atMost(1));
+const whitespace = optWhitespace.then(comment.atMost(1));
 
-var lexeme = function(p){ return p.skip(whitespace); }
+const lexeme = function(p){ return p.skip(whitespace); }
 
-var colon = lexeme(string(':'));
+const colon = lexeme(string(':'));
 
-var lbrace = lexeme(string('{'))
+const lbrace = lexeme(string('{'))
   , rbrace = lexeme(string('}'));
 
-var stripFirstLast = function(x) {
+const stripFirstLast = function(x) {
   return x.substr(1, x.length-2);
 };
 
-var identifier = lexeme(regex(/[a-zA-Z_][0-9a-zA-Z_+-]*/));
-var doubleString = lexeme(regex(/\"([^\"\\]|\\.)*\"/).map(stripFirstLast));
-var singleString = lexeme(regex(/\'([^\'\\]|\\.)*\'/).map(stripFirstLast));
+const identifier = lexeme(regex(/[a-zA-Z_][0-9a-zA-Z_+-]*/));
+const doubleString = lexeme(regex(/\"([^\"\\]|\\.)*\"/).map(stripFirstLast));
+const singleString = lexeme(regex(/\'([^\'\\]|\\.)*\'/).map(stripFirstLast));
 
-var number = lexeme(regex(/[.]?[0-9+-][0-9a-zA-Z_.+-]*/)).map(Number);
-var trueLiteral = lexeme(string('true')).result(true);
-var falseLiteral = lexeme(string('false')).result(false);
+const number = lexeme(regex(/[.]?[0-9+-][0-9a-zA-Z_.+-]*/)).map(Number);
+const trueLiteral = lexeme(string('true')).result(true);
+const falseLiteral = lexeme(string('false')).result(false);
 
-var expr = lazy('an expression', function() { return alt(pair, message).many(); });
+const expr = lazy('an expression', function() { return alt(pair, message).many(); });
 
-var message = seq(identifier, colon.times(0, 1).then(lbrace).then(expr).skip(rbrace))
+const message = seq(identifier, colon.times(0, 1).then(lbrace).then(expr).skip(rbrace))
                 .map(function(message) {
                   return { type: 'message', name: message[0], values: message[1] };
                 });
 
-var value = alt(trueLiteral, falseLiteral, number, doubleString, singleString, identifier);
+const value = alt(trueLiteral, falseLiteral, number, doubleString, singleString, identifier);
 
-var pair = seq(identifier.skip(colon), value)
+const pair = seq(identifier.skip(colon), value)
             .map(function(pair) {
               return { type: 'pair', name: pair[0], value: pair[1] };
             });
 
 module.exports = function(input) {
-  var result = whitespace.then(expr).parse(input);
+  const result = whitespace.then(expr).parse(input);
   if (!result.status) {
     result.error = Parsimmon.formatError(input, result);
   }
--- test/test.js
+++ test/test.js
@@ -1,19 +1,18 @@
 "use strict";
 
-var assert = require('assert')
-  , fs = require('fs');
+const assert = require('assert') , fs = require('fs');
 
-var ProtoBuf = require('protobufjs');
+const ProtoBuf = require('protobufjs');
 
-var sut = require('..');
+const sut = require('..');
 
 describe('parse', function() {
   it('should successfully load the basic prototype text format', async function() {
-    var fqn = 'caffe.NetParameter';
-    var input = fs.readFileSync('./test/basic.prototxt', 'utf-8');
+    const fqn = 'caffe.NetParameter';
+    const input = fs.readFileSync('./test/basic.prototxt', 'utf-8');
     
-    var builder = await (new ProtoBuf.Root()).load('./test/caffe.proto', { keepCase: true })
-    var result = sut.parse(builder, fqn, input)
+    const root = await (new ProtoBuf.Root()).load('./test/caffe.proto', { keepCase: true })
+    const result = sut.parse(root, fqn, input)
       , message = result.message;
 
     assert.equal(true, result.status);
@@ -21,6 +20,23 @@ describe('parse', function() {
     assert.equal(1, message.layers.length);
     assert.equal('data', message.layers[0].name);
     assert.equal('IMAGE_DATA', message.layers[0].type);
+  });
+});
+
+
+describe('parse googlefont', function() {
+  it('should successfully load the basic googlefont pb text format', async function() {
+    const fqn = 'google.fonts_public.FamilyProto';
+    const input = fs.readFileSync('./test/METADATA.pb', 'utf-8');
+    
+    const root = await (new ProtoBuf.Root()).load('./test/fonts_public.proto', { keepCase: true })
+    const result = sut.parse(root, fqn, input), message = result.message;
+
+    assert.equal(true, result.status);
+    assert.equal('Roboto', message.name);
+    assert.equal('italic', message.fonts[1].style);
+    assert.equal(75.0, message.axes[0].min_value);
+    assert.equal('wght', message.axes[1].tag);
   });
 });
 
